b87ae98277c9a15bf83ab529dd0b1b335b2088e6
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2021-04-23 13:43:47 -0700

    Create linkcheck.sh

---
+++ hack/linkcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+set -o pipefail
+readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )
+
+for f in "${DIR}"/**/*.md; do
+  docker run --rm -v /:/tmp:ro -i -w /tmp ghcr.io/tcort/markdown-link-check:stable "/tmp/${f}" || exit 1
+done
